05541912b379a5584ff9558086fc32c9935a543a
yashraj <yashrajsingh.dg@gmail.com>
2025-03-10 17:53:47 +0530

    Cors origin update

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 15
 }
-CORS_ALLOWED_ORIGINS = CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 
 # CORS_ALLOW_ALL_ORIGINS = True
 
