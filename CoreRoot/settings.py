a4c2485efa586556a480361400374b5c9d709f11
yashraj <yashrajsingh.dg@gmail.com>
2025-03-10 14:00:16 +0530

    CI CD Fixes

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -48,9 +48,6 @@ INSTALLED_APPS = [
     # external packages apps
     'rest_framework',
     'rest_framework_simplejwt',
-
-    'corsheaders',
-
     'core',
     'core.user',
     'core.auth',
@@ -62,7 +59,6 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
